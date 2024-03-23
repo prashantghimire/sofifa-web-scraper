d870dc04a9cbecea8d4ba37a96db1acb4a410874
Prashant Ghimire <pghimire6@gatech.edu>
2024-03-23 11:13:31 -0500

    bug fix, improvement

--- download.js
+++ download.js
@@ -146,7 +146,7 @@ async function getPlayerDetailsRow(url) {
 
     // 6. country_id,country_name,country_league_id,country_league_name,country_flag,country_rating,country_position,country_kit_number,
     function getPlayerNationalTeam() {
-        let results = ['', '', '', '', '', '', ''];
+        let results = ['', '', '', '', '', '', '', ''];
         const index = $(content_player_info).find('h5').map((i, el) => $(el).text().includes('National team')).get().indexOf(true);
         if (index < 0) {
             return results;
@@ -177,8 +177,8 @@ async function getPlayerDetailsRow(url) {
 
     function getPlayerAttributes() {
         // 7. player_attributes
-        const player_attr_grid1 = $(grids[2]).find('.col p').map((i, el) => $(el).text()).get();
-        const player_attr_grid2 = $(grids[3]).find('.col p').map((i, el) => $(el).text()).get();
+        const player_attr_grid1 = $(grids[2]).find('.col p').each((i, e) => $(e).find('span.plus').remove()).map((i, el) => $(el).text()).get();
+        const player_attr_grid2 = $(grids[3]).find('.col p').each((i, e) => $(e).find('span.plus').remove()).map((i, el) => $(el).text()).get();
 
         // attacking
         // crossing,finishing,heading_accuracy,short_passing,volleys
@@ -319,7 +319,7 @@ async function getPlayerDetailsRow(url) {
 }
 
 (async function () {
-    const row = await getPlayerDetailsRow('https://sofifa.com/player/231747/kylian-mbappe/240033/');
+    const row = await getPlayerDetailsRow('https://sofifa.com/player/278046/pau-cubarsi-paredes/240033/');
     const row_header = `player_id|version|full_name|description|image|height|weight|dob|positions|overall_rating|potential|value|wage|preferred_foot|weak_foot|skill_moves|international_reputation|work_rate|body_type|real_face|release_clause|specialities|club_id|club_name|club_league_id|club_league_name|club_logo|club_rating|club_position|club_kit_number|club_joined|club_contract_valid_until|country_id|country_name|country_league_id|country_league_name|country_flag|country_rating|country_position|country_kit_number|crossing|finishing|heading_accuracy|short_passing|volleys|dribbling|curve|fk_accuracy|long_passing|ball_control|acceleration|sprint_speed|agility|reactions|balance|shot_power|jumping|stamina|strength|long_shots|aggression|interceptions|positioning|vision|penalties|composure|defensive_awareness|standing_tackle|sliding_tackle|gk_diving|gk_handling|gk_kicking|gk_positioning|gk_reflexes|play_styles\n`;
     fs.writeFileSync('./test.csv', row_header);
     fs.appendFileSync('./test.csv', row)
