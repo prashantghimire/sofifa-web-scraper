500e788602b72e61c51f532ce9885414841af737
Prashant Ghimire <pghimire6@gatech.edu>
2024-03-24 12:33:02 -0500

    refactor

--- main.js
+++ main.js
@@ -11,9 +11,22 @@ const scanType = process.argv[2];
 
 const row_header = `"player_id","version","name","full_name","description","image","height_cm","weight_kg","dob","positions","overall_rating","potential","value","wage","preferred_foot","weak_foot","skill_moves","international_reputation","work_rate","body_type","real_face","release_clause","specialities","club_id","club_name","club_league_id","club_league_name","club_logo","club_rating","club_position","club_kit_number","club_joined","club_contract_valid_until","country_id","country_name","country_league_id","country_league_name","country_flag","country_rating","country_position","country_kit_number","crossing","finishing","heading_accuracy","short_passing","volleys","dribbling","curve","fk_accuracy","long_passing","ball_control","acceleration","sprint_speed","agility","reactions","balance","shot_power","jumping","stamina","strength","long_shots","aggression","interceptions","positioning","vision","penalties","composure","defensive_awareness","standing_tackle","sliding_tackle","gk_diving","gk_handling","gk_kicking","gk_positioning","gk_reflexes","play_styles"\n`;
 
-(async function start() {
+async function download(fileToRead, fileToWrite) {
+    const playerUrlList = fs.readFileSync(fileToRead).toString().trim().split('\n');
+    fs.writeFileSync(fileToWrite, row_header, {flag: 'w'});
+
     let count = 0;
-    let isFullScan = scanType === 'full';
+    console.time('scan complete');
+    for (let url of playerUrlList) {
+        let row = await getPlayerDetailsCsvRow(url);
+        fs.writeFileSync(fileToWrite, row + '\n', {flag: 'a'});
+        console.log((++count) + '-' + url);
+    }
+    console.timeEnd('scan complete');
+}
+
+(async function start() {
+    const isFullScan = scanType === 'full';
     
     if (isFullScan){
         console.log('running full scan.')
@@ -23,15 +36,6 @@ const row_header = `"player_id","version","name","full_name","description","imag
 
     let fileToRead = isFullScan ? playerUrlsFullFile : playerUrlsTestFile;
     let fileToWrite = isFullScan ? playerDataFullFile : playerDataTestFile;
-    
-    let playerUrlList = fs.readFileSync(fileToRead).toString().trim().split('\n');
-    fs.writeFileSync(fileToWrite, row_header, {flag: 'w'});
 
-    console.time('scan complete');
-    for (let url of playerUrlList) {
-        let row = await getPlayerDetailsCsvRow(url);
-        fs.writeFileSync(fileToWrite, row + '\n', {flag: 'a'});
-        console.log((++count) + '-' + url);
-    }
-    console.timeEnd('scan complete');
+    await download(fileToRead, fileToWrite);
 }());
