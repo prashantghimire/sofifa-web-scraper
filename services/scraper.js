14e3c59489dc9dbdb55e9aa17d1f664466318b35
Prashant Ghimire <pras.ghim@gmail.com>
2024-10-11 07:48:24 -0500

    update test file

--- services/scraper.js
+++ services/scraper.js
@@ -1,21 +1,27 @@
 const Humanoid = require('humanoid-js');
 const humanoid = new Humanoid();
 
+const asyncWait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 /**
  * This method will read the content of any webpage.
  * @param url
  * @returns {Promise<String>}
  */
 const getPageContent = async (url) => {
-    const response = await humanoid.get(url);
-    if (response.statusCode !== 200) {
-        throw new Error(`Error reading page=${url}, statusCode=${response.statusCode}`);
+    let attempts = 5;
+    while (attempts > 0) {
+        const response = await humanoid.get(url);
+        if (response.statusCode !== 200) {
+            await asyncWait(1000);
+            attempts -= 1;
+        } else {
+            attempts = 5;
+            await asyncWait(300);
+            return response.body;
+        }
     }
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(response.body);
-        }, 300);
-    });
+    throw new Error(`Error reading page=${url}, statusCode=${response.statusCode}`);
 };
 
 module.exports = {
